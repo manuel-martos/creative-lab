0a1f3fbe008c34afaf04b077b283095f0385d03d
Manel Martos <manuel.martos-roldan@team.bumble.com>
2023-07-28 15:29:08 +0200

    Support KSP

---
+++ app/src/main/java/info/degirona/creativelab/test/TargetUiState.kt
@@ -0,0 +1,9 @@
+package info.degirona.creativelab.test
+
+import com.bumble.appyx.interactions.core.ui.property.impl.Alpha
+import com.bumble.appyx.interactions.core.ui.state.MutableUiStateSpecs
+
+@MutableUiStateSpecs
+class TargetUiState(
+    val alpha: Alpha.Target,
+)
